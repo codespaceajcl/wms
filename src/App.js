90a65bdef2b201123efe3deda5e54077dfac8201
hamza19mughal99 <hamza19mughal99@gmail.com>
2023-11-06 09:22:21 +0500

    boilerplate for warehouse management system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Home from "Container/Pages/Home/Home";
+import NotFound from "Container/Pages/NotFound/NotFound";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>AJCL WAREHOUSE MANAGEMENT SYSTEM</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/Container/Pages/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return <div>
+  <h1>NOT FOUND</h1>
+  </div>;
+};
+
+export default NotFound;
